b4010511c6eb3919ff917a7fd93b5127d248c412
Brian Pardy <brian.pardy@gmail.com>
2020-02-19 12:11:45 -0500

    Add script to clean up GISAID sequence file for direct use

---
+++ scripts/normalize_gisaid_fasta.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e 
+GISAID_SARSCOV2_IN=$1
+GISAID_SARSCOV2_OUT=$2
+
+echo "Normalizing GISAID file $GISAID_SARSCOV2_IN to $GISAID_SARSCOV2_OUT"
+
+# Remove leading 'BetaCoV' and 'BetaCov' from sequence names
+# Convert embedded spaces in sequence names to underscore (Hong Kong sequences)
+# Remove trailing |EPI_ISL_id|datestamp from sequence names
+# Eliminate duplicate sequences (keep only the first seen)
+
+cat $GISAID_SARSCOV2_IN | 
+	sed 's/^>BetaCoV\//>/gi' |	# remove leading BetaCo[vV]
+	sed 's/ /_/g' |			# remove embedded spaces
+	sed 's/|.*$//' | 		# remove trailing metadata
+	awk 'BEGIN{RS=">";FS="\n"}!x[$1]++{print ">"$0}' | # remove duplicates
+	grep -v '^>*$' > $GISAID_SARSCOV2_OUT
+
+
